5c4671ab0dd86898f6a454e95170287a7abf6780
Gabriel Barros <gabriel.fbarros@yahoo.com.br>
2019-02-20 21:11:44 -0300

    implements simple file upload.

---
+++ src/ManutencaoPreventiva/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ManutencaoPreventiva.Models;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+
+namespace ManutencaoPreventiva.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public IActionResult Index(List<IFormFile> files)
+        {
+            foreach (var file in files)
+            {
+                if (file.Length > 0)
+                {
+                    var result = new List<string>();
+                    using (var reader = new StreamReader(file.OpenReadStream()))
+                    {
+                        while (reader.Peek() >= 0)
+                            result.Add(reader.ReadLine());
+                    }
+                }
+            }
+
+            return Ok();
+        }
+    }
+}
